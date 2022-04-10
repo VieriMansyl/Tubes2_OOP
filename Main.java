04a15f81e1f074aacaa815f614eacc6dd936d598
SurTan02 <suryanto.tan5@gmail.com>
2022-04-10 19:59:21 +0700

    add template

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main  {
+
+    public static void main(String[] args) {
+        System.out.println(("JavaFX Version: " + VersionInfo.getVersion()));
+        System.out.println(("JavaFX Runtime Version: " + VersionInfo.getRuntimeVersion()));
+    }
+}
